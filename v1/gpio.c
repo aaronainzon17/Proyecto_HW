0ae2e6892b0d3f3a468fbf9abd95c419dcfcffc4
Aaron Ibañez Espes <aaronainzon@gmail.com>
2021-11-18 13:10:51 +0100

    Update gpio.c

--- v1/gpio.c
+++ v1/gpio.c
@@ -13,13 +13,17 @@ void GPIO_iniciar(void){
 */
 int GPIO_leer(int bit_inicial, int num_bits){
 	int res = 0;
-	int j = 1, i,value;
-	for (i = bit_inicial; i < (bit_inicial+num_bits); i++){
+	int value;
+	int peso = 1;
+	int max = bit_inicial + num_bits;
+	int i = bit_inicial;
+	while (i < max){
 		value = (IOPIN & (1<<i));
 		if (value != 0){
-				res += j;
+				res += peso;
 		}
-		j*=2;
+		peso*=2;
+		i++;
 	}
 	return res;
 }
@@ -31,27 +35,32 @@ int GPIO_leer(int bit_inicial, int num_bits){
 */
 void GPIO_escribir(int bit_inicial, int num_bits, int valor){
 	int resto;
-	int i = bit_inicial ;
-	for (i = bit_inicial; i < (bit_inicial+num_bits); i++){
+	int i = bit_inicial;
+	int max = bit_inicial + num_bits;
+	while (i < max){
 		resto = valor % 2;
 		valor /= 2;
 		if (resto == 1){
 			//Para poner a 1 se pone en IOSET
-			IOSET |= 1<<i;
+			IOSET = IOSET | 1<<i;
 		}else{
 			// Para poner a 0 un bit como en IOSET o no hace nada 
 			// se limpia el valor en IOCLR
-			IOCLR |= 1<<i;
+			IOCLR = IOCLR | 1<<i;
 		}
+		i++;
 	}
 }
 
 void GPIO_marcar_entrada(int bit_inicial, int num_bits){
 	uint32_t ini = 0xFFFFFFFF;
 	uint32_t bits = (ini << bit_inicial); //Se introducen ceros por detras hasta el bit inicial 
-	int i;
-	for (i=31; i>=(bit_inicial+num_bits); i--){//desde el bit final pongo ceros hasta el ultimo bit
-		bits &= ~(1<<i);
+	int i = 31; //Max bits de la GPIO
+	int limit = bit_inicial+num_bits;
+	while (i>=limit){
+		//desde el bit final pongo ceros hasta el ultimo bit
+		bits = bits & ~(1<<i);
+		i--;
 	}
 	bits = ~bits; //lo invierto todo
 	IODIR = IODIR & bits; //Pongo cero los nuevos y el resto se quedan como antes
@@ -59,10 +68,13 @@ void GPIO_marcar_entrada(int bit_inicial, int num_bits){
 
 void GPIO_marcar_salida(int bit_inicial, int num_bits){
 	uint32_t ini = 0xFFFFFFFF;
-	uint32_t bits = (ini << bit_inicial); //meto ceros por detr?s hasta el bit inicial 
-	int i;
-	for (i=31; i>=(bit_inicial+num_bits); i--){//desde el final pongo ceros
-		bits &= ~(1<<i);
+	uint32_t bits = (ini << bit_inicial); //Se introducen ceros por detras hasta el bit inicial 
+	int i = 31; //Max bits de la GPIO
+	int limit = bit_inicial+num_bits;
+	while (i>=limit){
+		//desde el bit final pongo ceros hasta el ultimo bit
+		bits = bits & ~(1<<i);
+		i--;
 	}
 	IODIR =  IODIR | bits; //Pongo a uno los nuevos
 }
